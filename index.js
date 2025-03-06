6e82c38e8ad2b4f195eab6cbc795ad531cfccc6a
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-03-06 23:20:03 +0600

    Explore all the global Objects

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-var http = require('http');
-http.createServer(function(req, res){
-    res.writeHead(200, {'Content-Type' : 'text/plain'});
-    res.end("Hello guys");
-}).listen(4000);
+// var http = require('http');
+// http.createServer(function(req, res){
+//     res.writeHead(200, {'Content-Type' : 'text/plain'});
+//     res.end("Hello guys");
+// }).listen(4000);
 
+
+setTimeout(()=>{
+    console.log('test');
+}, 1000);
+
+setInterval(()=>{
+    console.log("Hello good people");
+}, 2000)
