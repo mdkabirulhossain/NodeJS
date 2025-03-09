48a65c341b66fd08f06cc518b56c9c577fae18a8
mdkabirulhossain <mdkabirulhossainj@gmail.com>
2025-03-10 00:13:56 +0600

    Write text one file to another file using pipe

--- index.js
+++ index.js
@@ -194,6 +194,10 @@ const fs = require('fs');
 const ourReadStream = fs.createReadStream(`${__dirname}/bigdata.txt`, 'utf8');
 const ourWriteStream = fs.createWriteStream(`${__dirname}/output.txt`, 'utf8');
 
-ourReadStream.on('data', (data) =>{
-    ourWriteStream.write(data);
-})
+// ourReadStream.on('data', (data) =>{
+//     ourWriteStream.write(data);
+// })
+
+//Write data using pipe
+
+ourReadStream.pipe(ourWriteStream);
